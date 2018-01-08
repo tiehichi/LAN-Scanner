d39ead01ff63a795a2f774b826d187433531d46c
Tiehichi <daibingzhi9519@outlook.com>
2018-01-08 15:23:28 +0800

    add "update" option to update oui.txt

--- lanscanner.sh
+++ lanscanner.sh
@@ -1,27 +1,42 @@
 #! /bin/sh
 
-ipreg='\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}'
+scan(){
 
-ips=$(ip addr show | grep $ipreg | awk '{print $2}')
+	ipreg='\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}'
 
-for ip in $ips
-do
-    if [ "$ip" = "127.0.0.1/8" ]; then
-        continue
-    fi
+	ips=$(ip addr show | grep $ipreg | awk '{print $2}')
 
-    lan_ips=$(nmap -sP $ip | grep $ipreg | sed 's/[^0-9\.]//g')
-    
-    for lan_ip in $lan_ips
-    do
-        if [ "$lan_ip" = "$(echo $ip | sed 's/\/[0-9]\{1,2\}//g')" ]; then
-            continue
-        fi
+	for ip in $ips
+	do
+		if [ "$ip" = "127.0.0.1/8" ]; then
+			continue
+		fi
 
-        mac=$(grep "$lan_ip[[:space:]]" /proc/net/arp | awk '{print $4}')
-        mac=$(echo $mac | tr 'a-z' 'A-Z')
-        oui=$(echo $mac | cut -c 1-8 | tr ':' '-')
-        manu=$(grep $oui ./oui.txt | awk '{print $3}' FS='\t')
-        echo "$lan_ip\t$mac\t$manu"
-    done
-done
+		lan_ips=$(nmap -sP $ip | grep $ipreg | sed 's/[^0-9\.]//g')
+
+		for lan_ip in $lan_ips
+		do
+			if [ "$lan_ip" = "$(echo $ip | sed 's/\/[0-9]\{1,2\}//g')" ]; then
+				continue
+			fi
+
+			mac=$(grep "$lan_ip[[:space:]]" /proc/net/arp | awk '{print $4}')
+			mac=$(echo $mac | tr 'a-z' 'A-Z')
+			oui=$(echo $mac | cut -c 1-8 | tr ':' '-')
+			manu=$(grep $oui ./oui.txt | awk '{print $3}' FS='\t')
+			echo "$lan_ip\t$mac\t$manu"
+		done
+	done
+}
+
+update(){
+	echo "Update oui.txt, please wait..."
+	curl http://standards-oui.ieee.org/oui.txt > ./out.txt
+	echo "Update complete"
+}
+
+if [ "$1" = "update" ]; then
+	update
+else
+	scan
+fi
